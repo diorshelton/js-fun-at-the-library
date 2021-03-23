1a864631690ea98ecf9e36c5eb820c4cfb7941a4
Dior.Shelton <dior.shelton@gmail.com>
2021-03-22 20:58:13 -0400

    Pass all tests in book-test.js

--- src/book.js
+++ src/book.js
@@ -1,21 +1,40 @@
 function createTitle(title) {
-  return `The ${title}`;
-}
-function buildMainCharacter(name, age, pronouns) {
-  return {
-    name: `${name}`,
-    age: `${age}`,
-    pronouns: `${pronouns}`,
+  if (title.includes('The')){
+    return `${title}`
+  } else { 
+    return `The ${title}`;
   }
 }
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+}
+
 function saveReview(review, reviews) {
   if (!reviews.includes(review)) {
   reviews.push(review)};
 }
 
 function calculatePageCount(bookTitle) {
-  return bookTitle.length * 20;
+  return (bookTitle.length * 20);
 }
+
+function writeBook(title, mainCharacter, genre) {
+  return {
+  title:createTitle(title),
+  mainCharacter:mainCharacter,
+  pageCount:calculatePageCount(title), 
+  genre:genre,
+  };
+}
+
+function editBook(book) {
+  return book.pageCount = (book.pageCount * .75);
+ }
 
 module.exports = {
   createTitle,
@@ -23,5 +42,5 @@ module.exports = {
   saveReview,
   calculatePageCount,
   writeBook,
-  // editBook
+  editBook
 }
